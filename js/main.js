8b7a6f8d4cc37b77f4163ec932285e625be556fe
DESKTOP-VEVQNJG\sutti <srangseethammakul@gmail.com>
2021-04-17 20:15:59 +0700

    test js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 (() => {
-    function run(){
+    async function getAirQuality() {
+        const response = await fetch( `https://ecom.suttipongact.info/api/getProvince`);
+        const { provinces } = await response.json();
+        provinces.map(function(province) {
+            return console.log(province.province_name);
+        });
+    }
+    async function run(){
+        await getAirQuality();
         console.log("function run");
     }
     run();
