38d8f09b87e1cf2492c6d8ea4a9e37a55d967c55
DESKTOP-VEVQNJG\sutti <srangseethammakul@gmail.com>
2021-04-17 20:23:05 +0700

    init line

--- js/main.js
+++ js/main.js
@@ -3,11 +3,29 @@
         const response = await fetch( `https://ecom.suttipongact.info/api/getProvince`);
         const { provinces } = await response.json();
         provinces.map(function(province) {
-            return console.log(province.province_name);
+            let x = document.createElement("P");
+            let t = document.createTextNode(province.province_name);
+            x.appendChild(t);
+            document.body.appendChild(x);
         });
     }
     async function run(){
         await getAirQuality();
+        liff.ready.then(() => {
+            document.getElementById("isLoggedIn").append(liff.isLoggedIn())
+            if (liff.isLoggedIn()) {
+            //   getEnvironment()
+            //   getUserProfile()
+            //   getContext()
+            //   getFriendship()
+            //   createUniversalLink()
+            } else {
+              liff.login()
+            }
+          })
+          await liff.init({
+            liffId: "1655804673-q9X5v3KA"
+        });
         console.log("function run");
     }
     run();
